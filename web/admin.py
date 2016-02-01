8e687a76c3e03080df8afd869f5cfaf54ed6236f
Alex Li <alexli@micai.asia>
2016-02-01 19:31:00 +0800

    update home page

--- web/admin.py
+++ web/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+
+import models
+
+class NotificationLinksAdmin(admin.ModelAdmin):
+    list_display=('priority', 'category','title','hide','date','class_open_date')
+    list_editable = ('hide','class_open_date')
+admin.site.register(models.NotificationLinks,NotificationLinksAdmin)

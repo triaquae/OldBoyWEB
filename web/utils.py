7c9dfe4526e5dd74de3c5672c5585e281a6f55c6
薛保库 <xuebk@jxjr.com>
2016-03-29 16:23:45 +0800

    1.修复 在没有任何客服时候.产生的异常 2.增加 系统增日志

--- web/utils.py
+++ web/utils.py
@@ -3,19 +3,32 @@ __author__ = 'Alex Li'
 import random
 import models
 import datetime
+import logging
+logger = logging.getLogger(__name__)
 
 def allowcate_consultant():
+    u"""
+    分配 客服
+    :return:
+    """
     match_dic ={
-        0:'monday',
-        1:'tuesday',
-        2:'wednesday',
-        3:'thursday',
-        4:'friday',
-        5:'saturday',
-        6:'sunday'
+        0: 'monday',
+        1: 'tuesday',
+        2: 'wednesday',
+        3: 'thursday',
+        4: 'friday',
+        5: 'saturday',
+        6: 'sunday'
     }
-    c_weekday =datetime.datetime.now().weekday()
-    consultant_list= models.Consultants.objects.filter(**{match_dic[c_weekday]:True})
-    pick_one = consultant_list[random.randrange(len(consultant_list))]
-    #print('pick consult:',pick_one)
+    pick_one = None
+    c_weekday = datetime.datetime.now().weekday()
+    try:
+        consultant_list = models.Consultants.objects.filter(**{match_dic[c_weekday]:True})
+        if not consultant_list:
+            raise Exception(u'当前客服列表为空.请注意!!!')
+        pick_one = consultant_list[random.randrange(len(consultant_list))]
+        if pick_one is not None:
+            raise Exception(u'当前客服列表整形失败')
+    except Exception as e:
+        logger.warning(e.message)
     return pick_one
